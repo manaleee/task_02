e20b2167833bfc5f448059e2dfb810a51e4a3747
Manal Alkandari <manal_alkandri@hotmail.com>
2020-01-12 20:26:31 +0300

    done

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def manal(request):
+    context = {
+        "msg": "Hello World!",
+        "hello":1
+    }
+    return render(request, 'manal.html', context)
